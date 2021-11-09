3e271b56f15a4a43830620020785f840df6beac9
Vijayraj-wd <vijayrajck@outlook.com>
2021-11-09 18:55:34 +0530

    Initial commit

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,37 +1,5 @@
-import { useState } from "react";
-
 const Content = () => {
-  const [Items, setItems] = useState([]);
-  const [Value, setValue] = useState("");
-
-  const handleSubmit = () => {
-    setItems((prev) => [...prev, Value]);
-    setValue(" ");
-  };
-
-  const handleValue = (e) => {
-    setValue(e.target.value);
-  };
-
-  const handleDelete = (e) => {
-    setItems((prev) => prev.filter((i) => i !== e));
-  };
-
-  return (
-    <div>
-      <input value={Value} onChange={handleValue} type="text"></input>
-      <button type="submit" onClick={handleSubmit}>
-        Submit
-      </button>
-      <ul>
-        {Items.map((item) => (
-          <li>
-            {item} <button onClick={() => handleDelete(item)}>Delete</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div>Hooks</div>;
 };
 
 export default Content;
