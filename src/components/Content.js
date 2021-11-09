3816d7ab0a70c5d92047b7f928a6653283dea65e
Vijayraj-wd <vijayrajck@outlook.com>
2021-11-09 19:32:54 +0530

    React Hooks: useEffect

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,38 @@
+import { useState, useEffect } from "react";
+
 const Content = () => {
-  return <div>Hooks</div>;
+  const [Counter, setCounter] = useState(0);
+  const [Content, setContent] = useState([]);
+
+  const ApiCall = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/photos");
+    const data = await res.json();
+    setContent(data);
+  };
+
+  useEffect(() => {
+    ApiCall();
+  }, []);
+
+  return (
+    <div>
+      <h1>{Counter}</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus quas
+        unde corrupti fuga. Temporibus rerum quis nobis dicta assumenda
+        necessitatibus aut! Accusamus quibusdam accusantium nisi repellendus
+        nulla, autem nemo impedit.
+      </p>
+      <button type="submit" onClick={() => setCounter((prev) => prev + 1)}>
+        Click
+      </button>
+      <ul>
+        {Content.map((item) => (
+          <li>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Content;
